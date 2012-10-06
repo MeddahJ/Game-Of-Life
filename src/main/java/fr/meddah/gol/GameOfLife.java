a7967d2b31f1c1d807916ba93fb4a4912bfad86a
MEDDAH Julien <meddah.julien@gmail.com>
2012-10-06 15:37:39 +0200

    Entry point mutable but much simpler

--- src/main/java/fr/meddah/gol/GameOfLife.java
+++ src/main/java/fr/meddah/gol/GameOfLife.java
@@ -1,68 +1,44 @@
 package fr.meddah.gol;
 
 import java.util.Iterator;
-import java.util.Set;
 
 import static ch.lambdaj.Lambda.*;
 import static fr.meddah.gol.Pattern.*;
 import static fr.meddah.gol.Tools.*;
 import static java.util.Arrays.*;
 
-public class GameOfLife implements Iterable<GameOfLife> {
+public class GameOfLife implements Iterator<GameOfLife> {
 
 	public static void main(String[] args) {
-		forEach(firstRound(SPACESHIP, GLIDER, BLOCK)).play();
+		forEach(firstRound(GLIDER)).play();
 	}
 
 	static GameOfLife firstRound(Pattern... patterns) {
-		Set<Cell> cells = flatMap(asList(patterns), on(Pattern.class).getCells());
-		return new GameOfLife(null, new Board(cells));
+		return new GameOfLife(new Board(flatMap(asList(patterns), on(Pattern.class).getCells())));
 	}
 
 	void play() {
 		System.out.println(board);
 	}
 
-	boolean canContinue() {
-		return nextBoard.hasCells();
-	}
-
-	GameOfLife continues() {
-		return new GameOfLife(nextBoard, nextBoard.next());
-	}
-
-	@Override
-	public Iterator<GameOfLife> iterator() {
-		return new GameIterator(this);
-	}
-
-	private GameOfLife(Board board, Board nextBoard) {
-		this.board = board;
-		this.nextBoard = nextBoard;
-	}
-
-	private final Board board, nextBoard;
-}
-
-class GameIterator implements Iterator<GameOfLife> {
-
 	@Override
 	public boolean hasNext() {
-		return game.canContinue();
+		return nextBoard.hasCells();
 	}
 
 	@Override
 	public GameOfLife next() {
-		game = game.continues();
-		return game;
+		board = nextBoard;
+		nextBoard = nextBoard.next();
+		return this;
 	}
 
 	@Override
 	public void remove() {}
 
-	GameIterator(GameOfLife game) {
-		this.game = game;
+	private GameOfLife(Board nextBoard) {
+		this.nextBoard = nextBoard;
 	}
 
-	private GameOfLife game;
+	private Board board, nextBoard;
 }

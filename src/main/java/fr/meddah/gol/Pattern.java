4ad866dbd82142e05891a49a3fb7cc3ea3f42111
MEDDAH Julien <julien@meddah.fr>
2012-10-23 23:32:19 +0200

    public enum Pattern implements Iterable<Cell>

--- src/main/java/fr/meddah/gol/Pattern.java
+++ src/main/java/fr/meddah/gol/Pattern.java
@@ -4,40 +4,34 @@ import java.util.Iterator;
 
 import static java.util.Arrays.*;
 
-public class Pattern implements Iterable<Cell> {
+public enum Pattern implements Iterable<Cell> {
 
-	public static final Pattern
-			CELL = pattern(cell(0, 0)),
-			BLINKER = pattern(cell(0, -1), cell(0, 0), cell(0, 1)),
-			BLOCK = pattern(cell(0, 0), cell(0, 1), cell(1, 0), cell(1, 1)),
-			GLIDER = pattern(cell(0, 0), cell(0, 1), cell(0, 2), cell(1, 2), cell(2, 1)),
-			SPACESHIP = pattern(cell(0, 0), cell(0, 2), cell(1, 3), cell(2, 3),
-					cell(3, 0), cell(3, 3), cell(4, 1), cell(4, 2), cell(4, 3));
+	CELL(cell(0, 0)),
+	BLINKER(cell(0, -1), cell(0, 0), cell(0, 1)),
+	BLOCK(cell(0, 0), cell(0, 1), cell(1, 0), cell(1, 1)),
+	GLIDER(cell(0, 0), cell(0, 1), cell(0, 2), cell(1, 2), cell(2, 1)),
+	SPACESHIP(cell(0, 0), cell(0, 2), cell(1, 3), cell(2, 3), cell(3, 0), cell(3, 3), cell(4, 1), cell(4, 2), cell(4, 3)); 
 
-	public Pattern from(int x, int y) {
-		return new Pattern(cell(x, y).cellsFrom(cells));
+	public Iterable<Cell> from(int x, int y) {
+		return cell(x, y).cellsFrom(relativeCells);
 	}
 
-	public Pattern next() {
-		return new Pattern(new Board(cells).next());
-	}
-
-	private Pattern(Iterable<Cell> cells) {
-		this.cells = cells;
+	public Iterable<Cell> nextFrom(int x, int y) {
+		return new Board(from(x, y)).next();
 	}
 
 	@Override
 	public Iterator<Cell> iterator() {
-		return cells.iterator();
-	}
-
-	private final Iterable<Cell> cells;
-
-	private static Pattern pattern(Cell... cells) {
-		return new Pattern(asList(cells));
+		return relativeCells.iterator();
 	}
 
 	private static Cell cell(int x, int y) {
 		return new Cell(x, y);
 	}
+
+	private Pattern(Cell... cells) {
+		this.relativeCells = asList(cells);
+	}
+
+	private final Iterable<Cell> relativeCells;
 }
